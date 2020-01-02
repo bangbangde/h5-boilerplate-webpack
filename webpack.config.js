f81ab5bfba4b95ff7e2837387b00504f9b2ac17e
frank <chengqifw@gmail.com>
2020-01-02 20:35:53 +0800

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+    // port: 8080,
+    // open: true,
+    hot: false,
+    // https: true,
+    // writeToDisk: true,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3000',
+    //     secure: false
+    //   }
+    // }
+  },
   entry: './src/javascripts/index.js',
   output: {
     filename: 'bundle.js',
@@ -12,10 +27,21 @@ module.exports = {
   module: {
     rules: [
       {
+        //样式
         test: /\.css$/,
         use: [
           'style-loader', 'css-loader'
         ]
+      },
+      {
+        //图片
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        //字体
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
       }
     ]
   },
